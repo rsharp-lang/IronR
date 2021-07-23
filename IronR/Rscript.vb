6c994b66f1859ddf7e806ef2407996489da18ec0
guigang xie <gg.xie@bionovogene.com>
2021-07-23 14:54:24 +0800

    calling python

--- IronR/Rscript.vb
+++ IronR/Rscript.vb
@@ -1,3 +1,17 @@
-﻿Public Class Rscript
+﻿Imports Microsoft.Scripting.Hosting
 
-End Class
+Public Module Rscript
+
+    ReadOnly eng As ScriptEngine = IronPython.Hosting.Python.CreateEngine()
+    ReadOnly scope As ScriptScope = eng.CreateScope()
+
+    Sub New()
+        eng.Execute("
+            def greetings(name):
+                return 'Hello ' + name.title() + '!'
+            ", scope)
+
+        Dim greetings As Func(Of String, String) = scope.GetVariable("greetings")
+        Console.WriteLine(greetings("world"))
+    End Sub
+End Module
---
+++ test/Program.vb
@@ -0,0 +1,21 @@
+﻿Imports IronPython.Runtime
+Imports Microsoft.Scripting.Hosting
+
+Module Program
+
+    ReadOnly eng As ScriptEngine = IronPython.Hosting.Python.CreateEngine()
+    ReadOnly scope As ScriptScope = eng.CreateScope()
+
+    Sub Main()
+        eng.Execute("
+def greetings(name):
+    return 'Hello ' + name.title() + '!'
+", scope)
+
+        Dim greetings = scope.GetVariable("greetings")
+
+        Console.WriteLine(greetings("world"))
+        Pause()
+    End Sub
+
+End Module
