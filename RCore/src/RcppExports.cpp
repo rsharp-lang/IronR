cd0b8d64299bb32afb552a09b2a48d802f170c31
xieguigang <xie.guigang@gcmodeller.org>
2021-09-20 13:50:33 +0800

    now  working on windows?

--- RCore/src/RcppExports.cpp
+++ RCore/src/RcppExports.cpp
@@ -5,9 +5,24 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
+// netcore5_init0
+int netcore5_init0();
+RcppExport SEXP _IronR_netcore5_init0() {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    rcpp_result_gen = Rcpp::wrap(netcore5_init0());
+    return rcpp_result_gen;
+END_RCPP
+}
 // rcpp_hello_world
 List rcpp_hello_world();
-RcppExport SEXP _testpack_rcpp_hello_world() {
+RcppExport SEXP _IronR_rcpp_hello_world() {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -17,11 +32,12 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_testpack_rcpp_hello_world", (DL_FUNC) &_testpack_rcpp_hello_world, 0},
+    {"_IronR_netcore5_init0", (DL_FUNC) &_IronR_netcore5_init0, 0},
+    {"_IronR_rcpp_hello_world", (DL_FUNC) &_IronR_rcpp_hello_world, 0},
     {NULL, NULL, 0}
 };
 
-RcppExport void R_init_testpack(DllInfo *dll) {
+RcppExport void R_init_IronR(DllInfo *dll) {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
 }
---
+++ RCore/src/init0.cpp
@@ -0,0 +1,12 @@
+
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+int netcore5_init0() {
+    // initialize of the .NET core host at here
+    // call this function automatically in zzz.R
+    // when loading the R package
+    
+    return 0;
+}
